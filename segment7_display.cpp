e9c0c1653d213c69769864dd7576449b9af79275
Ashish Shakya <landxcape09@gmail.com>
2019-11-20 16:05:23 +0545

    l

--- segment7_display.cpp
+++ segment7_display.cpp
@@ -39,61 +39,51 @@ void segment7_display::number_display(char number, bool decimalSet = false)
 {
     digitalWrite(latchPin, LOW); // Latch
     setData(decimalSet);
+
     if (number == '0')
-    {
         for (uint8_t i = 0; i < 7; i++)
             setData(segment_map[0][6 - i]);
-    }
+
     else if (number == '1')
-    {
         for (uint8_t i = 0; i < 7; i++)
             setData(segment_map[1][6 - i]);
-    }
+
     else if (number == '2')
-    {
         for (uint8_t i = 0; i < 7; i++)
             setData(segment_map[2][6 - i]);
-    }
+
     else if (number == '3')
-    {
         for (uint8_t i = 0; i < 7; i++)
             setData(segment_map[3][6 - i]);
-    }
+
     else if (number == '4')
-    {
         for (uint8_t i = 0; i < 7; i++)
             setData(segment_map[4][6 - i]);
-    }
+
     else if (number == '5')
-    {
         for (uint8_t i = 0; i < 7; i++)
             setData(segment_map[5][6 - i]);
-    }
+
     else if (number == '6')
-    {
         for (uint8_t i = 0; i < 7; i++)
             setData(segment_map[6][6 - i]);
-    }
+
     else if (number == '7')
-    {
         for (uint8_t i = 0; i < 7; i++)
             setData(segment_map[7][6 - i]);
-    }
+
     else if (number == '8')
-    {
         for (uint8_t i = 0; i < 7; i++)
             setData(segment_map[8][6 - i]);
-    }
+
     else if (number == '9')
-    {
         for (uint8_t i = 0; i < 7; i++)
             setData(segment_map[9][6 - i]);
-    }
+
     else
-    {
         for (uint8_t i = 0; i < 7; i++)
             setData(segment_map[10][6 - i]);
-    }
+
     digitalWrite(latchPin, HIGH); // Latch
 }
 
@@ -119,26 +109,28 @@ void segment7_display::setSelection(bool select = 1)
 
 void segment7_display::display_num_rows(String ROWS, bool first_row)
 {
+    char num, next_num;
     bool leading_zero = true;
-    char num;
-    int display_cols = ROWS.length();
+    uint8_t display_cols = ROWS.length();
 
-    for (int c = 0; c < display_cols; c++)
+    for (uint8_t c = 0; c < display_cols; c++)
     {
         num = ROWS[c];
+        next_num = ROWS[c + 1];
+
         clearBits();
         if (first_row && c == 0)
             setSelection(0);
         else
             setSelection(1);
 
-        if (leading_zero && num == '0' && ROWS[c + 1] != '.' && c != display_cols - 1)
+        if (leading_zero && num == '0' && next_num != '.' && c != display_cols - 1)
         {
         }
         else
         {
             leading_zero = false;
-            if (ROWS[c + 1] == '.')
+            if (next_num == '.')
             {
                 number_display(num, true);
                 c++;
@@ -154,8 +146,7 @@ void segment7_display::display_num_rows(String ROWS, bool first_row)
 void segment7_display::display_values(String *ROW_VALUES)
 {
     display_num_rows(*(ROW_VALUES), true);
-    for (int i = 1; i < nrows; i++)
-    {
+
+    for (uint8_t i = 1; i < nrows; i++)
         display_num_rows(*(ROW_VALUES + i));
-    }
 }
